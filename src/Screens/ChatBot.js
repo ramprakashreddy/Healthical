7a4884488ba1e4ec9afa11f4c60aa84567535c76
Zuber <zuber0264@gmail.com>
2021-07-03 18:09:36 +0530

    navigation changes

---
+++ src/Screens/ChatBot.js
@@ -0,0 +1,14 @@
+import React, { useCallback, useState, useEffect } from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Chatbot() {
+
+    return (
+        <View style={{ flex: 1, backgroundColor: '#eaf3fa' }}>
+            <Text>
+                Chatbot
+            </Text>
+        </View>
+    );
+
+}
